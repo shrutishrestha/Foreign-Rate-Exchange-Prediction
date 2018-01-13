5ed9a155c8db4522b261499e3996a2c108aec6f1
shrutishrestha <srutsth@gmail.com>
2018-01-13 23:06:42 +0545

    Forex prediction

--- final_sentiment original/lo.py
+++
@@ -1,8 +0,0 @@
-a=["a.csv","b","c"]
-x=["aink","blink"]
-for v in a:
-    for i in x:
-        if(v in i):
-            print("i love u")
-        else :
-           print("i hate u")
--- final_sentiment original/test.py
+++
@@ -1,9 +0,0 @@
-import re
-a="it not is that it doesnot decrease"
-# b=re.sub(r'([.,\s]|$)not', " ", a.csv)
-aa=a.count("not")
-if "not" in a:
-    print("ula")
-else:
-    print("ii")
-print(aa)
